12d9e16803974ac0d2113e18b0066e420cb47673
11707 <ethan.lubeck@vtsd.com>
2022-12-21 08:57:00 -0500

    note

---
+++ FirstArray/Example.java
@@ -0,0 +1,31 @@
+public class Example{
+    public static void main(String args[]){
+
+        //Arrays store one datatype
+        int numberOfChristmasGifts[];
+        numberOfChristmasGifts = new int [25];
+
+        for (int k= 0;  k< numberOfChristmasGifts.length; k++) {
+            numberOfChristmasGifts[k] = 6;
+
+            System.out.println( numberOfChristmasGifts[k]);
+        }
+
+
+
+        /*
+        numberOfChristmasGifts[0] = 7;
+        numberOfChristmasGifts[1] = 3;
+
+        System.out.println(numberOfChristmasGifts[0]);
+        System.out.println(numberOfChristmasGifts[1]);
+        */
+
+
+
+
+
+    }
+
+
+}
